b9b5fd970b9c3c0b56ed818e2889743cbab5469b
Mathieu Avila <mathieu.avila@gmail.com>
2021-02-14 21:13:21 +0100

    room structure definition

---
+++ generator/src/pygenerator/room_structure.py
@@ -0,0 +1,35 @@
+class RoomStructure:
+
+    """
+    Gives "interface" to what's must be implemented for a room structure
+    Should never been called here, will assert ("abstract type")
+    """
+
+    _name = "RoomStructure"
+
+    def __init__(self, room):
+        """ nothing"""
+        self._room = room
+
+    def check_structure(self, gate_list):
+        """ Pass the Room, and list of gates, check everything is as expected.
+            Should at least check that the gates are taken into account and at the right sizes
+        """
+        assert("Subclass me")
+
+    def check_fit(self, gate_list):
+        """ Pass the Room, and list of gates, check it can be applied. """
+        assert("Subclass me")
+
+    def instantiate(self, gate_list):
+        """ performs parameters selection. Parameters should be enough to generate specific file"""
+        assert("Subclass me")
+
+    def create_structure(self):
+        """ create from instance """
+
+    def load_structure_file(self, filename):
+        """ load structure from file """
+
+    def save_structure_file(self, filename):
+        """ dump all to file """
