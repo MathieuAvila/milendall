a3bf897d57fefb9e1159d8a17ac06134c555757f
Mathieu Avila <mathieu.avila@gmail.com>
2021-02-14 21:14:00 +0100

    room selector introduction

---
+++ generator/src/pygenerator/room_selector.py
@@ -0,0 +1,11 @@
+class RoomSelector:
+
+    def __init__(self, dict):
+        assert("Subclass me")
+
+    def get_room_fit(self, room, gate_list):
+        
+        assert("Subclass me")
+
+    def instantiate(self, Room, gate_list):
+        assert("Subclass me")
