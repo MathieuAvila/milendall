4709c6e1d683dc12bad8f93efca22b76b3dd340a
Mathieu Avila <mathieu.avila@gmail.com>
2021-05-08 16:07:41 +0200

    add gltf model unit

---
+++ game/test/unit_gltf_model.cxx
@@ -0,0 +1,14 @@
+#include "catch_amalgamated.hpp"
+
+#include <filesystem>
+#include <iostream>
+
+#include "gltf_model.hxx"
+
+TEST_CASE( "Load GLTF model", "[gltf]" ) {
+
+    auto fl = FileLibrary();
+    fl.addRootFilesystem(std::filesystem::current_path().c_str() + std::string("/../game/test/sample/cube"));
+    auto ref = fl.getRoot().getSubPath("room_preview.gltf");
+    GltfModel* model = new GltfModel(ref);
+}
