2861f09c621efbb4215aee556509d757c0c3b167
Mathieu Avila <mathieu.avila@gmail.com>
2021-05-08 16:08:00 +0200

    log all when in UT

--- game/test/unit_main.cpp
+++ game/test/unit_main.cpp
@@ -1,2 +1,13 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch_amalgamated.hpp"
+
+#include "common.hxx"
+
+struct ConsoleInit {
+    ConsoleInit()
+    {
+        set_level(level::debug);
+    }
+};
+
+ConsoleInit consoleInit;
