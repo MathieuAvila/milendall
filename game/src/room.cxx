7d9d7a5d5d361028ff319734cefebd0a462a1cfb
Mathieu Avila <mathieu.avila@laposte.net>
2021-07-09 23:01:53 +0200

    read room data - WIP

--- game/src/room.cxx
+++ game/src/room.cxx
@@ -6,6 +6,8 @@
 #include "common.hxx"
 #include "room.hxx"
 #include "gltf_mesh.hxx"
+#include "json_helper_accessor.hxx"
+#include "gltf_exception.hxx"
 
 static auto console = spdlog::stdout_color_mt("room");
 
@@ -20,8 +22,33 @@ struct RoomNode : public GltfNode
 RoomNode::RoomNode(nlohmann::json& json, GltfDataAccessorIFace* data_accessor) :
         GltfNode(json)
 {
+
+
     if (json.contains("extras")) {
-      console->info("Found application data for RoomNode");
+        auto extras = json["extras"];
+        auto points = jsonGetElementByName(extras, "points").get<int>();
+        console->info("Found application data (extras) for RoomNode, points are {}", points);
+        auto points_accessor = data_accessor->accessId(points);
+        if (points_accessor->unit_type != points_accessor->FLOAT)
+            throw GltfException("invalid data type, except float");
+        if (points_accessor->vec_type != points_accessor->VEC3)
+            throw GltfException("invalid unit type, except VEC3 for points");
+        float* raw_data = (float*)(points_accessor->data);
+        console->info("points count {}", points_accessor->count);
+        for (auto i = 0; i < points_accessor->count; i++)
+        {
+            console->info("point {}, [ {} {} {} ]", i, raw_data[i*3], raw_data[i*3 + 1], raw_data[i*3 + 2]);
+        }
+
+        jsonExecuteAllIfElement(extras, "phys_faces", [this, data_accessor](nlohmann::json& phys, int node_index) {
+            auto data = jsonGetElementByName(phys, "data");
+            auto type = jsonGetElementByName(data, "type").get<string>();
+            auto accessor = jsonGetElementByName(phys, "accessor").get<int>();
+            console->info("found phys_face:{} with accessor={}, type is {}", to_string(data), accessor, type);
+            auto node_data = data_accessor->accessId(accessor);
+
+            uint16_t* raw_data = (uint16_t*)(node_data->data);
+        });
     }
 }
 
