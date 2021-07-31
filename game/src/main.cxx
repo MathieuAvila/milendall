73549a3a42c37e88fdf99689f8bff5084701d7ef
Mathieu Avila <mathieu.avila@laposte.net>
2021-07-31 14:48:21 +0200

    align main to preview code to fix compilation

--- game/src/main.cxx
+++ game/src/main.cxx
@@ -2,45 +2,122 @@
 #define GLM_FORCE_MESSAGES
 #include <glm/glm.hpp>
 
-// Include GLEW
 #include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
-#include "level.hxx"
-
-#include <memory>
-#include <iostream>
-#include <stdio.h>
-#include <string>
-#include <vector>
-#include <iostream>
-#include <fstream>
-#include <algorithm>
 #include <filesystem>
+
+#include "common.hxx"
+#include "level.hxx"
+#include "gl_init.hxx"
+
+#include <stdlib.h>
+#include <unistd.h>
 
 using namespace std;
 
-// Include standard headers
-#include <stdio.h>
-#include <stdlib.h>
-
-// Include GLFW
-#include <GLFW/glfw3.h>
+static auto console = spdlog::stdout_color_mt("room_preview");
 
 // Include GLM
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-
-#include "gl_init.hxx"
-
 using namespace glm;
 
-#include "common.hxx"
+void help()
+{
+    console->info("preview_room [-h] -d model_file [-f path ...]");
+    exit(1);
+}
 
-int main( void )
+tuple<string, vector<string> > readParams(int argc, char* argv[])
+{
+    string modelPath;
+    vector<string> libPaths;
+
+    int c;
+    while ((c = getopt (argc, argv, "f:d:h")) != -1)
+    {
+        switch (c)
+            {
+            case 'f':
+                modelPath = optarg;
+                break;
+            case 'd':
+                libPaths.push_back(optarg);
+                break;
+            case 'h':
+                help();
+                break;
+            default:
+                console->error("Unknown option {}", char(c));
+                help();
+            }
+    }
+    if (modelPath.empty())
+    {
+        console->error("Need to specify 1 model path", c);
+        help();
+    }
+    return tuple{modelPath, libPaths};
+}
+
+int main(int argc, char* argv[])
 {
     set_level(level::debug);
+
+    auto [modelPath, libPaths] = readParams(argc, argv);
+
     auto fl = FileLibrary();
+    for (auto p: libPaths)
+        fl.addRootFilesystem(p);
+
     milendall_gl_init(fl);
+
+    auto ref = fl.getRoot().getSubPath(modelPath);
+    GltfMaterialLibraryIfacePtr materialLibrary = GltfMaterialLibraryIface::getMaterialLibray();
+    //auto room = make_unique<Room>(materialLibrary, ref);
+    //room->applyTransform();
+    auto level = make_unique<Level>(ref);
+    auto room_ids = level.get()->getRoomNames();
+    level.get()->update(0.0);
+    auto current_room = room_ids.begin();
+    console->info("Set current room to {}", *current_room);
+
+    auto new_space_state = GLFW_RELEASE;
+    auto space_state = GLFW_RELEASE;
+
+	do{
+        unlockAllFbo();
+
+		// Use our shader
+		activateDefaultDrawingProgram();
+
+		// Compute the MVP matrix from keyboard and mouse input
+		computeMatricesFromInputs();
+
+		setMeshMatrix(glm::mat4(1.0));
+
+        level.get()->draw(*current_room, position, direction, up);
+
+		// Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+        // iterate through rooms with SPACE
+        new_space_state = glfwGetKey(window, GLFW_KEY_SPACE);
+        if ((new_space_state == GLFW_PRESS) && (space_state != new_space_state))
+        {
+            current_room++;
+            if (current_room == room_ids.end())
+                current_room = room_ids.begin();
+            console->info("Current room set to {}", *current_room);
+        }
+        space_state = new_space_state;
+
+	} // Check if the ESC key was pressed or the window was closed
+	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+		   glfwWindowShouldClose(window) == 0 );
+
     milendall_gl_close();
 
 	return 0;
