3dea54dad24292a5b0592bbbef207f94551aa6ad
Mathieu Avila <mathieu.avila@laposte.net>
2021-07-31 14:50:18 +0200

    WIP: math helper intersection methods

--- game/src/helper_math.hxx
+++ game/src/helper_math.hxx
@@ -32,3 +32,29 @@ glm::vec4 positionToVec4(glm::vec3);
 
 /** @brief Useful to append the extra W=0 */
 glm::vec4 vectorToVec4(glm::vec3);
+
+/** @brief Sphere trajectory intersection with plane.
+ * @param radius sphere radius
+ * @param position1 sphere start position
+ * @param position2 sphere end position
+ * @param plane plane equation
+ * @param intersect_point OUT point of intersection
+ * @returns TRUE if it intersects
+*/
+bool intersectSphereTrajectoryPlane(
+    glm::vec3 position1, glm::vec3 position2, float radius,
+    glm::vec4 plane,
+    glm::vec3& intersect_point
+    );
+
+/** @brief Sphere trajectory intersection with segment.
+ * @param radius sphere radius
+ * @param position1 sphere start position
+ * @param position2 sphere end position
+ * @param plan plane equation
+ * @returns TRUE if it intersects
+*/
+bool intersectSphereTrajectorySegment(
+    glm::vec3 position1, glm::vec3 position2, float radius,
+    glm::vec4 plane
+    );
