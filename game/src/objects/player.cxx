ff729aadff7bfb045fc7c8535a4146265ca56439
MathieuAvila <mathieu.avila@gmail.com>
2022-08-28 20:17:29 +0200

    missing time value init

--- game/src/objects/player.cxx
+++ game/src/objects/player.cxx
@@ -6,7 +6,7 @@
 
 static auto console = getConsole("player");
 
-Player::Player() : movable_definition(MovableObjectDefinition(0.7f, 1.0f, 1.0f, 0.3f))
+Player::Player() : movable_definition(MovableObjectDefinition(0.7f, 1.0f, 1.0f, 0.3f)), time_left(0.0f)
 {
 
 }
