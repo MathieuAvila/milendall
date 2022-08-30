50370a86609fd2d9c6af1cbe95f255ba998c493c
MathieuAvila <mathieu.avila@gmail.com>
2022-08-30 18:48:00 +0200

    basic dressing, allow to set textures

--- generator/pygenerator/src/dressings/basic.py
+++ generator/pygenerator/src/dressings/basic.py
@@ -9,6 +9,8 @@ from dressing import Dressing
 from .register import register_dressing_type
 import gltf_helper
 
+from jsonmerge import merge
+
 logger = logging.getLogger("basic")
 logger.setLevel(logging.INFO)
 
@@ -19,7 +21,7 @@ class DressingBasic(Dressing):
     def __init__(self, element=None):
         """ init dressing """
         #super().__init__(element)
-        self.element = element
+        self._element = element
 
     def get_instance(self, element:None):
         """ return a self instance of this dressing, operating on any object"""
@@ -28,7 +30,15 @@ class DressingBasic(Dressing):
     def instantiate(self, selector):
         """ force set values:
         - set default values to dressing"""
-        self.element.values.parameters.dressing_private={}
+        dressing_parameters = self._element.values.parameters.dressing_parameters
+        my_default = {
+                concrete_room.Node.HINT_GROUND : "/common/basic/ground.jpg",
+                concrete_room.Node.HINT_WALL : "/common/basic/wall.jpg",
+                concrete_room.Node.HINT_CEILING : "/common/basic/ceiling.jpg",
+                concrete_room.Node.HINT_DOOR : "/common/basic/door.jpg"
+        }
+        self._element.values.parameters.dressing_private = merge( my_default, dressing_parameters)
+
 
     def generate(self, concrete):
         """Perform instantiation on concrete_room"""
@@ -37,12 +47,16 @@ class DressingBasic(Dressing):
 
             # get each kind of walls and associate a texture.
 
-            for kind_texture, texture, axes in [
-                [concrete_room.Node.HINT_GROUND, "/common/basic/ground.jpg", [["x"], ["z"]] ],
-                [concrete_room.Node.HINT_WALL, "/common/basic/wall.jpg",  [["x","z"], ["y"]] ],
-                [concrete_room.Node.HINT_CEILING, "/common/basic/ceiling.jpg", [["x"], ["z"]] ],
-                [concrete_room.Node.HINT_DOOR, "/common/basic/door.jpg", [["x"], ["z"]] ]
-                ]:
+            axes_map = { concrete_room.Node.HINT_GROUND:  [["x"], ["z"]] ,
+                concrete_room.Node.HINT_WALL:  [["x","z"], ["y"]] ,
+                concrete_room.Node.HINT_CEILING: [["x"], ["z"]] ,
+                concrete_room.Node.HINT_DOOR: [["x"], ["z"]]  }
+
+            private = self._element.values.parameters.dressing_private
+
+            for kind_texture in private.keys():
+                texture = private[kind_texture]
+                axes = axes_map[kind_texture]
                 list_faces = obj.get_visual_face([kind_texture])
                 logger.debug("kind: %s, texture:%s , len:%i",
                             kind_texture, texture, len(list_faces))
