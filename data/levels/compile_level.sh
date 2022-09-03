4cc62f39d6653c842b2ebcde11759d130d16138b
MathieuAvila <mathieu.avila@gmail.com>
2022-09-03 14:06:24 +0200

    fix compilation script

--- data/levels/compile_level.sh
+++ data/levels/compile_level.sh
@@ -7,4 +7,8 @@ SCRIPT_DIR=$(dirname $PWD/$0 )
 
 cd $SCRIPT_DIR/../../generator/pygenerator/src
 python ./generator.py -p -o /tmp/$SAMPLE_DIR $SAMPLE
+if [ "$?" != "0" ]
+then
+    exit 1
+fi
 echo "=== Generated in: /tmp/$SAMPLE_DIR"
