86f87e1d6f9b1f5760fe96863e95c032d6496d8d
MathieuAvila <mathieu.avila@gmail.com>
2022-09-16 21:25:32 +0200

    Add parametric brick

---
+++ generator/pygenerator/src/bricks/parametric.py
@@ -0,0 +1,125 @@
+"""
+structure definition for a fully parametric brick
+"""
+
+import logging
+from brick_structure import BrickStructure
+import concrete_room
+import cgtypes.vec3
+import cgtypes.mat4
+import functools
+
+from .register import register_brick_type
+
+from math import *
+
+from jsonmerge import merge
+
+logger = logging.getLogger("parametric")
+logger.setLevel(logging.INFO)
+
+class BrickParametric(BrickStructure):
+
+    _name = "parametric"
+
+    def __init__(self, _element=None):
+        """ init room """
+        self._element = _element
+
+    def get_instance(self, room:None):
+        """Return an instante"""
+        return BrickParametric(room)
+
+    def check_fit(self):
+        """ Pass the Room, and list of gates, check it can be applied. """
+        logger.info("checking if parametric fits: only if exactly 2 gates")
+        return 100
+
+    def check_structure(self):
+        """check everything is as expected.
+        """
+        logger.info("checking if tubular is ok. Only if 2 gates")
+        return len(self._element.gates) == 2
+
+    def instantiate(self, selector):
+        """ force set values:
+        - set values to room size
+        - set values for gates"""
+
+        structure_parameters = self._element.values.parameters.structure_parameters
+
+        my_default = {
+            "setup": {
+                "segments_nr_s": 5,                                 # 5 segments
+                "segments_nr_t": 5,                                 # 5 segments
+                "func": [
+                    "[",
+                    "s * 10.0, 0.0, t * 10.0, s, t",  # p(s,t) : x,y,z,u,v
+                    "]" ]}
+        }
+
+        self._element.values.parameters.structure_private = merge( my_default, structure_parameters)
+        logger.info("setup: %s", str(self._element.values.parameters.structure_private))
+
+    def generate(self, concrete):
+        """Perform instantiation on concrete_room"""
+        structure_private = self._element.values.parameters.structure_private
+
+        setup = structure_private["setup"]
+        parent = concrete.add_child(None, "parent")
+
+        points = []
+        indexes = []
+        counter = 0
+
+        nr_s = setup["segments_nr_s"]
+        nr_t = setup["segments_nr_t"]
+        func_array = setup["func"]
+        func = functools.reduce(lambda a, b: a + b, func_array )
+
+
+        for i in range(0, nr_s):
+            s = float(i) / float(nr_s)
+            index_raw = []
+            for j in range(0, nr_t):
+                t = float(j) / float(nr_s)
+                values = eval(func)
+                if len(values) != 5:
+                    raise "func must return a table of 5"
+                segment = []
+                print(values)
+                p = cgtypes.vec3(values[0], values[1], values[2])
+                p.u = values[3]
+                p.v = values[4]
+                p.has_tex = True
+                print(p)
+                index_raw.append(counter)
+                points.append(p)
+                counter = counter + 1
+            indexes.append(index_raw)
+
+        index_wall = parent.add_structure_points(points)
+
+        table = []
+
+        for i in range(0, nr_s -1):
+            s = float(i) / float(nr_s)
+            points_raw = []
+            for j in range(0, nr_t -1 ):
+                table.append([
+                    indexes[i]   [j+1],
+                    indexes[i+1] [j+1],
+                    indexes[i+1] [j],
+                    indexes[i]   [j]])
+
+        parent.add_structure_faces(
+                    0,
+                    table,
+                    concrete_room.Node.CAT_PHYS_VIS,
+                    [concrete_room.Node.HINT_GROUND, concrete_room.Node.HINT_BUILDING],
+                    {concrete_room.Node.PHYS_TYPE : concrete_room.Node.PHYS_TYPE_HARD} )
+
+
+
+
+register_brick_type(BrickParametric())
