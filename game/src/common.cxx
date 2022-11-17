4655fd54be2f01363cf406817e32370fd0081dea
MathieuAvila <mathieu.avila@gmail.com>
2022-11-17 19:22:34 +0100

    Fix UT all in a row, because spdlog does strange things when logger is not immediately accessed :/

--- game/src/common.cxx
+++ game/src/common.cxx
@@ -3,13 +3,13 @@
 std::shared_ptr<spdlog::logger> getConsole(std::string name)
 {
     auto result = spdlog::stdout_color_mt(name);
-    /*spdlog::get(name)->set_level(
+    spdlog::get(name)->set_level(
 #ifdef DEBUG
     spdlog::level::debug
 #else
     spdlog::level::info
 #endif
-    );*/
+    );
     return result;
 };
 
